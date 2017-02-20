4c6d049159b0d206101ee73e14bf16e4cc7f1f02
Eri Irawan <eryirawan91@gmail.com>
2017-02-20 13:14:27 +0700

    done

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  let say = req.query.say
+  let result = req.query.result
+  res.render('users',{title: 'Express',answer:''});
 });
+
+router.post('/', function(req, res, next){
+  let inputUser = req.body.result
+  if (inputUser.trim().split(' ').length >= 2) {
+    res.render('users',{answer:req.body.result.toLowerCase()})
+  }else {
+    res.render('users',{answer:"input kurang dari 2 kata"});
+  }
+})
 
 module.exports = router;
