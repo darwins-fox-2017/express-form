b62ff903366d1ac2a9a95619e40531d020435ef7
Diky Arga <dikyarga.id@gmail.com>
2017-02-20 11:40:49 +0700

    finish release 2

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
   res.send('isi paramater say is : ' + req.query.say);
+
 });
+
+router.get('/create', function(req, res, next){
+  res.render('users/create', { title: 'Create' });
+
+})
+
+router.post('/create', function(req, res, next){
+  res.send('Your input is : firstname : ' + req.body.firstname + ' lastname : ' + req.body.lastname)
+})
 
 module.exports = router;
